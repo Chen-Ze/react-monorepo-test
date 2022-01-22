2f9912fc334ec95e86b87cf3e4fd516e0fa66803
Chen Ze <chenze_ustc@mail.ustc.edu.cn>
2022-01-22 22:15:02 +0800

    build(src/play.ts): test error

--- src/play.ts
+++ src/play.ts
@@ -1,6 +1,7 @@
-function test(x: number) {
+function test(x): number {
     console.log('test called');
     return x + 1;
 }
 
 console.log(test(2));
+console.log(1);
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-function test(x: number) {
-    return x + 1;
-}
-
-console.log(test(2));
